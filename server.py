e051b30847633f898d6977a2a18ffcb4477e4cd3
jhylands <hylands.james@gmail.com>
2021-04-01 09:52:26 +0100

    Added the ability to retrive previous messages from the server

--- server.py
+++ server.py
@@ -8,6 +8,31 @@ import pymysql.cursors
 import pymysql
 
 
+@app.route('/last/<int:k>', methods=['GET'])
+def last_k_entries(k):
+    try:
+        # https://stackoverflow.com/a/34503728/1320619
+        # Connect to the database
+        connection = pymysql.connect(host="141.136.33.223",
+                                     user='timepcou_site',
+                                     password=os.environ["code"],
+                                     db='timepcou_devopchallenge',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "select * from store order by id desc limit %s"
+            acc = []
+            cursor.execute(sql, (k,))
+            for result in cursor.fetchall():
+                acc.append(result)
+
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connection.commit()
+    finally:
+        connection.close()
+    return jsonify(acc)
 
 @app.route('/store', methods=['POST'])
 def index():
