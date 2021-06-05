b41a84c82e4de9c9cfb0d07a82654e6e312503e4
jhylands <hylands.james@gmail.com>
2021-06-05 08:17:46 +0100

    backup

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
+from getpass import getpass
 import requests
 import json
 
 if __name__=="__main__":
+    session = requests.Session()
+    email = input("email:")
+    password = getpass()
+    session.post("http://chat.timep.co.uk/login", data={"email": email, "password":password})
     headers = {'Content-type': 'application/json'}
     while True:
         message = input(">")
         if message:
-            response = requests.post("http://localhost:8008/store", headers=headers, data=json.dumps({"message": message}))
+            response = session.post("http://chat.timep.co.uk/store_message", headers=headers, data=json.dumps({"message": message}))
             if response.status_code==200:
                 print(response.text)
             else:
+                print(response.text)
                 print("Some kind of issue has occurred")
